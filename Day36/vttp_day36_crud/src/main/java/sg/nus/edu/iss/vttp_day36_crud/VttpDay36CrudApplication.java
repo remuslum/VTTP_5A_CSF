18ac1df9d74b3d823f06cc706a7a3dab0308fbcd
remuslum <57741494+remuslum@users.noreply.github.com>
2025-03-11 23:03:23 +0800

    SQL Data

---
+++ Day36/vttp_day36_crud/src/main/java/sg/nus/edu/iss/vttp_day36_crud/VttpDay36CrudApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp_day36_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay36CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay36CrudApplication.class, args);
+	}
+
+}
---
+++ Day36/vttp_day36_crud/src/test/java/sg/nus/edu/iss/vttp_day36_crud/VttpDay36CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp_day36_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay36CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
